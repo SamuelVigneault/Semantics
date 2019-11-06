827c80bfe80b863272a1126c515c83b91db9fe78
Samuel Vigneault <samuelvigneault@samuels-mbp-3.hamilton.edu>
2019-11-06 18:18:00 -0500

    edit mac

--- decafs.cc
+++ decafs.cc
@@ -303,8 +303,8 @@ else if (tree->description == "class") {
     for (size_t i=0; i < tree->children.size(); i++) {
     	cout << "5555555" << endl;
     	if (tree->children[i]->description == "functiondecl") {
-    		if (dynamic_cast<S_function *>(currentSS->local_lookup(tree->children[i]->children[1]->token->text))) {
-    			currentFunc = dynamic_cast<S_function *>(currentSS->local_lookup(tree->children[i]->children[1]->token->text));
+    		if (dynamic_cast<S_function *>(currentSS->local_lookup(tree->children[3]->children[i]->children[1]->token->text))) {
+    			currentFunc = dynamic_cast<S_function *>(currentSS->local_lookup(tree->children[3]->children[i]->children[1]->token->text));
 				currentSS = tree->children[i]->symtab;
 				functionhandler();
 }
